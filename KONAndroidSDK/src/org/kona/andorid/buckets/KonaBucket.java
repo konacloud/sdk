64b2487e3e6a08c72345446f612904912f6b80cf
santiago <sjcotto@gmail.com>
2014-09-02 17:55:57 -0300

    some changes

--- KONAndroidSDK/src/org/kona/andorid/buckets/KonaBucket.java
+++ KONAndroidSDK/src/org/kona/andorid/buckets/KonaBucket.java
@@ -3,40 +3,25 @@
  */
 package org.kona.andorid.buckets;
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.ByteArrayBody;
 import org.apache.http.entity.mime.content.ContentBody;
-import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.HTTP;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.kona.andorid.HTTPUtil;
-import org.kona.andorid.KonaResponse;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -82,17 +67,12 @@ public class KonaBucket {
 					HttpClient client = new DefaultHttpClient(httpParams);
 					HttpPost post = new HttpPost(url);
 
-					List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-
 					ContentBody mimePart = new ByteArrayBody(bytes, "filename");
 
 					MultipartEntity reqEntity = new MultipartEntity(
 							HttpMultipartMode.BROWSER_COMPATIBLE);
 					reqEntity.addPart("filename", mimePart);
 
-					// post.setEntity(new UrlEncodedFormEntity(pairs,
-					// HTTP.UTF_8)); // as
-					// UTF-8
 					post.setEntity(reqEntity);
 					HttpResponse response = client.execute(post);
 
